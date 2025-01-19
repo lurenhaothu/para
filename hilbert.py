09095b22f30863793da53203c95784adcad13446
Renhao Lu <38303642+lurenhaothu@users.noreply.github.com>
2025-01-18 23:40:25 -0500

    .

--- hilbert.py
+++ hilbert.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-from model.MI.steerable import SteerablePyramid
+from model.MI.SPMI import SPMILoss
 #from steerable import SteerablePyramid
 import model.loss as loss
 
@@ -11,45 +11,17 @@ import math
 from model.MI.SPMap import SPMap
 
 import numpy as np
-'''
-path = "C:/Users/Renhao Lu/Desktop/dwt/test.jpg"
 
-imgg = torchvision.transforms.Grayscale(num_output_channels=1)(torchvision.transforms.ToTensor()(Image.open(path)))
+path1 = "C:/Users/Renhao Lu/Desktop/Skea_topo-main/Para/data/masks/000.png"
+path2 = "C:/Users/Renhao Lu/Desktop/Skea_topo-main/Para/data/masks/001.png"
 
-C, H, W = imgg.shape
+img1 = torchvision.transforms.Grayscale(num_output_channels=1)(torchvision.transforms.ToTensor()(Image.open(path1))).unsqueeze(0).cuda()
+img2 = torchvision.transforms.Grayscale(num_output_channels=1)(torchvision.transforms.ToTensor()(Image.open(path1))).unsqueeze(0).cuda()
 
-sp = SteerablePyramid(imgSize=400, K=4, N=4)
+loss1 = SPMILoss(complex=True, spN=4, spK=12)
+loss2 = SPMILoss(complex=False, spN=4, spK=12)
 
-print(sp.hl0.shape)
+print(loss1(img1, img2, None, torch.reshape(torch.tensor([1,1]), (1,2,1)).cuda()))
 
-fig, axe = plt.subplots(2,2)
-axe[0][0].imshow(sp.hl0[0,0,0,0,...].squeeze().cpu().numpy())
-axe[0][1].imshow(sp.hl0[0,0,0,1,...].squeeze().cpu().numpy())
-axe[1][0].imshow(torch.sum(torch.pow(sp.hl0, 2), dim=3).squeeze().cpu().numpy())
-#axe[1][1].imshow(im.angle().squeeze().cpu().numpy())
-plt.show()
-
-
-x = np.linspace(0, 2*np.pi, 1000)
-y = np.power(x, 2)
-filter1 = np.linspace(0, 1, 1000)
-#filter2 = np.sqrt(1 - np.power(filter1, 2))
-filter2 = 1 - filter1
-#filter1 = 0.5
-#filter2 = 0.5
-f1 = np.fft.fft(y) * filter2
-f2 = np.fft.fft(y) * filter1
-
-fig, axe = plt.subplots(3,3)
-axe[0][0].plot(x, filter1)
-axe[0][1].plot(x, filter2)
-axe[0][2].plot(x, np.power(filter1,2) + np.power(filter2, 2))
-axe[1][0].plot(x, np.abs(f1))
-axe[1][1].plot(x, np.abs(f2))
-axe[1][2].plot(x, np.fft.fft(y))
-axe[2][0].plot(x, np.abs(np.fft.ifft(f2)))
-axe[2][1].plot(x, np.abs(np.fft.ifft(f1) + np.fft.ifft(f2)))
-axe[2][2].plot(x, np.abs(np.fft.ifft(np.fft.fft(y))))
-plt.show()
-'''
+print(loss2(img1, img2, None, torch.reshape(torch.tensor([1,1]), (1,2,1)).cuda()))
 
--- model/MI/SPMI.py
+++ model/MI/SPMI.py
@@ -3,31 +3,29 @@ import torchvision
 # from model.MI.steerable import SteerablePyramid
 #from steerable import SteerablePyramid
 
-from ComplexSteerablePyramid import ComplexSteerablePyramid
+from model.MI.ComplexSteerablePyramid import ComplexSteerablePyramid
 import model.loss as loss
 
 from PIL import Image
 import matplotlib.pyplot as plt
 import math
 
-from model.MI.SPMap import SPMap
+# from model.MI.SPMap import SPMap
 
 _POS_ALPHA = 5e-4
 
 # 1-12-25 tested: 0.0001 SPMI + 0.01 BCE
 
 class SPMILoss(torch.nn.Module):
-    def __init__(self, imageSize, spN = 4, spK=4, beta=0.25, lamb=0.5, mag=1, map_method=2, map_weight=1, ffl: bool=False):
+    def __init__(self, complex=False, spN = 4, spK=4, beta=0.25, lamb=0.5, mag=1, ffl: bool=False):
         super(SPMILoss, self).__init__()
-        self.sp = ComplexSteerablePyramid(Complex=imgSize=imageSize, N=spN, K=spK)
+        self.sp = ComplexSteerablePyramid(complex=complex, N=spN, K=spK)
+        self.complex = complex
         self.beta = beta
         self.ffl = ffl
         self.lamb = lamb
         self.mag = mag
-        self.map_method = map_method
-        self.sp_map = SPMap()
-        self.map_weight = map_weight
-        self.BCEW = loss.Weight_Map_BCE()
+        self.BCEW = loss.BCE_withClassBalance()
 
     def forward(self, mask, pred, w_map, class_weight, epoch=None):
         if epoch == 0:
@@ -37,43 +35,58 @@ class SPMILoss(torch.nn.Module):
         mi_output = []
         for i in range(self.sp.N):
             if not self.ffl:
-                mi_output.append(torch.mean(self.mi(sp_mask[i + 1], sp_pred[i + 1])))
+                if self.complex:
+                    mi_output.append(torch.mean(self.complex_mi(sp_mask[i + 1], sp_pred[i + 1])).real)
+                else:
+                    mi_output.append(torch.mean(self.real_mi(sp_mask[i + 1], sp_pred[i + 1])))
             else:
                 mi_output.append(torch.mean(torch.log(torch.norm(sp_mask[i + 1] - sp_pred[i + 1], dim=1))))
-        with torch.no_grad():
-            if self.map_method == 2:
-                w_map = self.sp_map(sp_mask) + self.sp_map(sp_pred)
-                w_map = w_map * self.map_weight
-            elif self.map_method == 1:
-                w_map = self.sp_map(sp_mask) * self.map_weight
-            else:
-                w_map = None
+        print(mi_output)
         loss = self.BCEW(mask, pred, None, class_weight) * self.lamb
-        #for i in range(self.sp.N):
-        #    loss += math.pow(self.beta, self.sp.N - i - 1) * mi_output[i] * self.mag
+        for i in range(self.sp.N):
+            loss += math.pow(self.beta, self.sp.N - i - 1) * mi_output[i] * self.mag
         return loss
 
-    def mi(self, mask, pred):
-        # print(mask.shape)
-        B, C, H, W = mask.shape
-        mask_flat = mask.view(B, C, H * W).type(torch.cuda.DoubleTensor)
-        mask_mean = torch.mean(mask_flat, dim=2, keepdim=True)
+    def real_mi(self, mask, pred):
+        B, C, A, H, W = mask.shape # A: angle, number of orientations of the steerable pyramid
+        mask_flat = mask.view(B, C, A, H * W).type(torch.cuda.DoubleTensor)
+        mask_mean = torch.mean(mask_flat, dim=3, keepdim=True)
         mask_centered = mask_flat - mask_mean
 
-        pred_flat = pred.view(B, C, H * W).type(torch.cuda.DoubleTensor)
-        pred_mean = torch.mean(pred_flat, dim=2, keepdim=True)
+        pred_flat = pred.view(B, C, A, H * W).type(torch.cuda.DoubleTensor)
+        pred_mean = torch.mean(pred_flat, dim=3, keepdim=True)
         pred_centered = pred_flat - pred_mean
 
-        var_mask = torch.matmul(mask_centered, torch.permute(mask_centered, (0, 2, 1)))
-        var_pred = torch.matmul(pred_centered, torch.permute(pred_centered, (0, 2, 1)))
-        cov_mask_pred = torch.matmul(mask_centered, torch.permute(pred_centered, (0, 2, 1)))
+        var_mask = torch.matmul(mask_centered, torch.permute(mask_centered, (0, 1, 3, 2)))
+        var_pred = torch.matmul(pred_centered, torch.permute(pred_centered, (0, 1, 3, 2)))
+        cov_mask_pred = torch.matmul(mask_centered, torch.permute(pred_centered, (0, 1, 3, 2)))
 
-        # print(var_mask, var_pred, cov_mask_pred)
-
-        diag_matrix = torch.eye(C).unsqueeze(0)
+        diag_matrix = torch.eye(A)
         inv_cov_pred = torch.inverse(var_pred + diag_matrix.type_as(var_pred) * _POS_ALPHA)
 
-        cond_cov_mask_pred = var_mask - torch.matmul(torch.matmul(cov_mask_pred, inv_cov_pred), torch.permute(cov_mask_pred, (0, 2, 1)))
+        cond_cov_mask_pred = var_mask - torch.matmul(torch.matmul(cov_mask_pred, inv_cov_pred), torch.permute(cov_mask_pred, (0, 1, 3, 2)))
+
+        chol = torch.linalg.cholesky(cond_cov_mask_pred)
+        return 2.0 * torch.sum(torch.log(torch.diagonal(chol, dim1=-2, dim2=-1) + 1e-8), dim=-1)
+    
+    def complex_mi(self, mask, pred):
+        B, C, A, H, W = mask.shape # A: angle, number of orientations of the steerable pyramid
+        mask_flat = mask.view(B, C, A, H * W).type(torch.complex128)
+        mask_mean = torch.mean(mask_flat, dim=3, keepdim=True)
+        mask_centered = mask_flat - mask_mean
+
+        pred_flat = pred.view(B, C, A, H * W).type(torch.complex128)
+        pred_mean = torch.mean(pred_flat, dim=3, keepdim=True)
+        pred_centered = pred_flat - pred_mean
+
+        var_mask = torch.matmul(mask_centered, torch.permute(mask_centered, (0, 1, 3, 2)).conj())
+        var_pred = torch.matmul(pred_centered, torch.permute(pred_centered, (0, 1, 3, 2)).conj())
+        cov_mask_pred = torch.matmul(mask_centered, torch.permute(pred_centered, (0, 1, 3, 2)).conj())
+
+        diag_matrix = torch.eye(A)
+        inv_cov_pred = torch.inverse(var_pred + diag_matrix.type_as(var_pred) * _POS_ALPHA)
+
+        cond_cov_mask_pred = var_mask - torch.matmul(torch.matmul(cov_mask_pred, inv_cov_pred), torch.permute(cov_mask_pred, (0, 1, 3, 2)).conj())
 
         chol = torch.linalg.cholesky(cond_cov_mask_pred)
         return 2.0 * torch.sum(torch.log(torch.diagonal(chol, dim1=-2, dim2=-1) + 1e-8), dim=-1)
